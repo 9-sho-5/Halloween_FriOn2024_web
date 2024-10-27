20cb1cb5164179ffbb6816bc905e8e0019225fad
RyoOuchi <ryoouchityrky@gmail.com>
2024-10-27 22:54:39 +0900

    Initial commit

---
+++ animate.js
@@ -0,0 +1,30 @@
+function toggleDropdown() {
+  const dropdownButton = document.querySelector(".dropdown button");
+  const dropdownMenu = document.getElementById("dropdownMenu");
+
+  dropdownMenu.classList.toggle("show");
+  dropdownButton.classList.toggle("active");
+}
+
+window.onclick = function(event) {
+  if (!event.target.closest('.dropdown')) {
+    const dropdowns = document.getElementsByClassName("dropdown-content");
+    const dropdownButton = document.querySelector(".dropdown button");
+
+    for (let i = 0; i < dropdowns.length; i++) {
+      const openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+        dropdownButton.classList.remove("active");
+      }
+    }
+  }
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+  const progressBars = document.querySelectorAll(".progress-bar");
+  progressBars.forEach(bar => {
+    const targetPercentage = parseInt(bar.getAttribute("data-target"));
+    bar.style.width = targetPercentage + "%"; 
+  });
+});
